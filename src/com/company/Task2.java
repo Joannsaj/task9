f383a841ef0c54769dc71b74a7530a706089e9a3
Joan <joannsaj@gmail.com>
2022-05-02 12:54:17 +0300

    ex9

---
+++ src/com/company/Task2.java
@@ -0,0 +1,32 @@
+package com.company;
+
+class Tasttwo{
+    public static void dependant(int val){
+        int array[]=new int[5];
+
+        try{
+            if(val==0){
+               int p= 3/val;
+            }else if (val==1){
+                array[6]=6;
+            }else {
+                return;
+            }
+        }catch (ArithmeticException e){
+            System.out.println("You can not divide by 0");
+            return;
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("That index does not exist");
+        }finally {
+            System.out.println("We are all good");
+        }
+    }
+}
+public class Task2 {
+    public static void main(String[] args) {
+        for(int i =0; i<3; i++){
+            Tasttwo.dependant(i);
+            System.out.println();
+        }
+    }
+}
